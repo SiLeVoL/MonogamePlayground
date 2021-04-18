2c2a063dd18752d00693f069f7b38e8b0981eb4e
Maico Weise <silevol@gmail.com>
2021-04-18 17:01:34 +0200

    added simple Menu

---
+++ monogame-playground/monogame-playground/Controls/Button.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace monogame_playground.Controls {
+    public class Button : Component {
+        #region Fields
+
+        private MouseState _currentMouse;
+        private MouseState _previousMouse;
+        private SpriteFont _font;
+        private bool _isHovering;
+        private Texture2D _texture;
+        private Vector2 _position;
+        private float _fontSize = 1;
+
+        #endregion
+
+        #region Properties
+
+        public event EventHandler Click;
+        public bool Clicked { get; private set; }
+        public Color PenColor { get; set; }
+        public Vector2 Position {
+            get => _position;
+            set => _position = new Vector2(value.X - Rectangle.Width / 2, value.Y - Rectangle.Height / 2);
+        }
+
+        public float FontSize {
+            get => _fontSize;
+            set => _fontSize = value;
+        }
+
+        public Rectangle Rectangle {
+            get { return new Rectangle((int) Position.X, (int) Position.Y, _texture.Width, _texture.Height); }
+        }
+
+        public string Text { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public Button(Texture2D texture, SpriteFont font) {
+            _texture = texture;
+            _font = font;
+            PenColor = Color.Black;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            var color = Color.White;
+
+            if (_isHovering) {
+                color = Color.Gray;
+            }
+
+            spriteBatch.Draw(_texture, Rectangle, color);
+
+            if (!string.IsNullOrEmpty(Text)) {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2 * FontSize);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2 * FontSize);
+
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColor, 0f, Vector2.Zero, Vector2.One * FontSize, SpriteEffects.None, 0f);
+            }
+        }
+
+        public override void Update(GameTime gameTime) {
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
+            _isHovering = false;
+            
+            if (Rectangle.Contains(_currentMouse.Position)) {
+                _isHovering = true;
+
+                if (_currentMouse.LeftButton == ButtonState.Released &&
+                    _previousMouse.LeftButton == ButtonState.Pressed) {
+                    Click?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- monogame-playground/monogame-playground/Game1.cs
+++ monogame-playground/monogame-playground/Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using monogame_playground.Controls;
 
 namespace monogame_playground
 {
@@ -8,6 +10,9 @@ namespace monogame_playground
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private GameState _state = GameState.MainMenu;
+        private Color _backgroundColor = Color.Black;
+        private List<Component> _gameComponents;
 
         public Game1()
         {
@@ -18,8 +23,10 @@ namespace monogame_playground
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth = 1920;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = 1080;   // set this value to the desired height of your window
+            _graphics.ApplyChanges();
+            
             base.Initialize();
         }
 
@@ -27,26 +34,119 @@ namespace monogame_playground
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            var playButton = new Button(Content.Load<Texture2D>("Controls/BiggerButton"),
+                Content.Load<SpriteFont>("Fonts/Font")) {
+                Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, 700),
+                Text = "Play",
+                FontSize = 2
+            };
+
+            playButton.Click += PlayButton_Click;
+
+            var quitButton = new Button(Content.Load<Texture2D>("Controls/BiggerButton"),
+                Content.Load<SpriteFont>("Fonts/Font")) {
+                Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, 800),
+                Text = "Quit",
+                FontSize = 2
+            };
+
+            quitButton.Click += QuitButton_Click;
+
+            _gameComponents = new List<Component>() {
+                playButton,
+                quitButton,
+            };
+        }
+
+        private void QuitButton_Click(object sender, System.EventArgs e) {
+            Exit();
+        }
+
+        private void PlayButton_Click(object sender, System.EventArgs e) {
+            _state = GameState.Play;
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            switch (_state) {
+                case GameState.MainMenu:
+                    UpdateMainMenu(gameTime);
+                    break;
+                case GameState.Pause:
+                    UpdatePause(gameTime);
+                    break;
+                case GameState.Play:
+                    UpdatePlay(gameTime);
+                    break;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            switch (_state) {
+                case GameState.MainMenu:
+                    DrawMainMenu(gameTime);
+                    break;
+                case GameState.Pause:
+                    DrawPause(gameTime);
+                    break;
+                case GameState.Play:
+                    DrawPlay(gameTime);
+                    break;
+            }
 
             base.Draw(gameTime);
         }
+
+        private void UpdateMainMenu(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Start == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Enter)) {
+                _state = GameState.Play;
+            }
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape)) {
+                Exit();
+            }
+            
+            foreach (var component in _gameComponents) {
+                component.Update(gameTime);
+            }
+        }
+
+        private void DrawMainMenu(GameTime gameTime) {
+            GraphicsDevice.Clear(_backgroundColor);
+
+            _spriteBatch.Begin();
+            
+            foreach (var component in _gameComponents) {
+                component.Draw(gameTime, _spriteBatch);
+            }
+            
+            _spriteBatch.End();
+        }
+        
+        private void UpdatePause(GameTime gameTime) {
+            
+        }
+
+        private void DrawPause(GameTime gameTime) {
+            
+        }
+        
+        private void UpdatePlay(GameTime gameTime) {
+            
+        }
+
+        private void DrawPlay(GameTime gameTime) {
+            GraphicsDevice.Clear(_backgroundColor);
+        }
+    }
+
+    enum GameState {
+        MainMenu,
+        Pause,
+        Play
     }
 }
