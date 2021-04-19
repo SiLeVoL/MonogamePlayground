509578494f1c903910a13768dca1cac572206e18
Maico Weise <silevol@gmail.com>
2021-04-19 14:17:48 +0200

    added camera class

---
+++ monogame-playground/monogame-playground/Gameplay/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using Microsoft.Xna.Framework;
+
+namespace monogame_playground.Gameplay {
+    public class Camera {
+        public Vector3 CamTarget { get; set; }
+        public Vector3 CamPosition { get; set; }
+        public Matrix ProjectionMatrix { get; }
+        public Matrix ViewMatrix { get; }
+
+        public Camera(Vector3 camTarget, Vector3 camPosition, GraphicsDeviceManager graphics) {
+            CamTarget = camTarget;
+            CamPosition = camPosition;
+
+            ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(45f), graphics.GraphicsDevice.Viewport.AspectRatio,
+                1f, 1000f);
+                
+            ViewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                                     new Vector3(0f, 1f, 0f));// Y up
+        }
+    }
+}
