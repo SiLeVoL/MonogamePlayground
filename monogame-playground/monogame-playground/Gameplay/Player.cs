530699885c98fca75d721d94d82272a5d218175b
Maico Weise <silevol@gmail.com>
2021-04-19 13:22:32 +0200

    added model and player class

---
+++ monogame-playground/monogame-playground/Gameplay/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+
+namespace monogame_playground.Gameplay {
+    
+    public class Player : Model {
+        private Model _player;
+        public Player Instance => this;
+
+        public Player(Model playerModel) {
+            _player = playerModel;
+        }
+    }
+}
---
+++ monogame-playground/monogame-playground/Model.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace monogame_playground {
+    public class Model {
+        public void Draw(GameTime gameTime) {
+            
+        }
+
+        public void Update(GameTime gameTime) {
+            
+        }
+    }
+}
