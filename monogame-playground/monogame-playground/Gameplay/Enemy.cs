89ed380061e920b88bcb0a12166d9d11cffbccf5
Maico Weise <silevol@gmail.com>
2021-04-19 17:08:13 +0200

    changed speeds to be independent from update rates

--- monogame-playground/monogame-playground/Gameplay/Enemy.cs
+++ monogame-playground/monogame-playground/Gameplay/Enemy.cs
@@ -14,9 +14,11 @@ namespace monogame_playground.Gameplay {
         
         public override void Update(GameTime gameTime, List<Game3DObject> entities, GameState gameState) {
             base.Update(gameTime, entities, gameState);
+            
+            float deltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
 
-            _position.Y -= .5f;
-            _position.X += (entities.Find(x => x.GetType() == typeof(Player)).Position.X - _position.X) * 0.05f;
+            _position.Y -= 40f * deltaTime;
+            _position.X += (entities.Find(x => x.GetType() == typeof(Player)).Position.X - _position.X) * 4f * deltaTime;
 
             if (_position.Y < -20) {
                 _position = new Vector3(0f, 40, 0f);
--- monogame-playground/monogame-playground/Gameplay/Player.cs
+++ monogame-playground/monogame-playground/Gameplay/Player.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Input;
 namespace monogame_playground.Gameplay {
     
     public class Player : Game3DObject {
-        private float speed = .3f;
+        private float speed = 20f;
         private float distance = 2;
 
         public Player(Model model) : base(model) {
@@ -16,19 +16,21 @@ namespace monogame_playground.Gameplay {
         
         public override void Update(GameTime gameTime, List<Game3DObject> entities, GameState gameState) {
             base.Update(gameTime, entities, gameState);
+            
+            float deltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
 
             // Movement
             if (Keyboard.GetState().IsKeyDown(Keys.Left)) {
-                _position.X += speed;
+                _position.X += speed * deltaTime;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right)) {
-                _position.X -= speed;
+                _position.X -= speed * deltaTime;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Up)) {
-                _position.Y += speed;
+                _position.Y += speed * deltaTime;
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Down)) {
-                _position.Y -= speed;
+                _position.Y -= speed * deltaTime;
             }
 
             foreach (Enemy enemy in entities.FindAll(x => x.GetType() == typeof(Enemy))) {
